3c06f5b13bff8ec4a1979b14d6f8282fe8f1f519
lihaitao03 <lihaitao03@baidu.com>
2017-08-24 18:00:33 +0800

    code 3d map，maprenderer

--- code 3d map/src/core/mapRenderer.js
+++ code 3d map/src/core/mapRenderer.js
@@ -9,6 +9,7 @@ define(function (require) {
     const THREE = require('three');
     const CURVE_ANCHORS = 4;
     const CURVE_SEGMENTS = 200;
+    const FOLDER_DISTANCE = 1000;
     const CURVE_COLORS = [
         0xd0d0d0, 0xff7575, 0xffaad5, 0xffa6ff, 0xd3a4ff, 0xE0E0E0, 0xFF9797, 0xFFC1E0, 0xFFBFFF,
         0xDCB5FF, 0xF0F0F0, 0xFFB5B5, 0xFFD9EC, 0xFFD0FF, 0xE6CAFF, 0xFCFCFC, 0xFFD2D2, 0xFFECF5,
@@ -28,48 +29,134 @@ define(function (require) {
         0xF2E6E6, 0xE8E8D0, 0xD1E9E9, 0xF3F3FA, 0xEBD3E8
     ];
 
+
     const dataset = require('../dataset');
     const fontLoader = new THREE.FontLoader();
-    
+    const {fileTreeConfig, fileMapConfig} = dataset;
+
 
     return function (me) {
-
-        fontLoader.load('resource/fonts/helvetiker_regular.typeface.json', function (font) {
-            const {renderer, scene} = me;
-            const file = fileFactory({
-                pos: new THREE.Vector3(500, 0, -500)
-            });
-            const folder = folderFactory({
-                pos: new THREE.Vector3(0, 0, 0)
-            });
-            const curve = curveFactory({
-                begin: new THREE.Vector3(0, 0, 0),
-                end: new THREE.Vector3(500, 0, -500)
-            });
-            const folderName = textFactory({
-                text: 'src',
-                font: font,
-                cameraAngleB: me.props.cameraAngleB,
-                isDirectory: true,
-                pos: new THREE.Vector3(0, 50, 0)
-            });
-            const fileName = textFactory({
-                text: 'main.js',
-                font: font,
-                cameraAngleB: me.props.cameraAngleB,
-                isDirectory: false,
-                pos: new THREE.Vector3(500, 50, -500)
-            });
-            scene.add(file);
-            scene.add(folder);
-            scene.add(folderName);
-            scene.add(fileName);
-            scene.add(curve);
-            me.text3dHash = [folderName, fileName];
-        });
-
+        fontLoader.load('resource/fonts/helvetiker_regular.typeface.json', (font) => drawMap(me, font));
     };
 
+
+
+    // 绘制地图
+    function drawMap(me, font) {
+
+        // 底层参数
+        const cameraAngleB = me.props.cameraAngleB;
+        const scene = me.scene;
+        // 记录每个节点的3D位置
+        let nodePositions = {
+            'src': new THREE.Vector3(0, 0, 0)
+        };
+        // 每个节点开始画圈时的角度起点
+        let nodeBeginA = {
+            'src': 0
+        };
+        // 所有文字节点
+        let text3dHash = [];
+        // 正在操作的队列
+        let reading = [];
+
+        // /////////////////////生成所有文件夹节点
+        // reading.push('src');
+        while(reading.length) {
+            // 创建正在遍历的节点
+            const folder = reading.shift();
+            const pos = nodePositions[folder];
+            const textPos = pos.clone();
+            textPos.y = 50;
+            const box = folderFactory({pos});
+            const text = textFactory({
+                text: folder.split('/').pop(),
+                font: font,
+                cameraAngleB: cameraAngleB,
+                isDirectory: true,
+                pos: textPos
+            });
+            scene.add(box);
+            scene.add(text);
+            text3dHash.push(text);
+            // 生成子目录数据
+            const child = findChildFolders(folder);
+            if (!child.length) continue;
+            const a = nodeBeginA[folder];
+            const d = 360 / child.length;
+            child.map(function (path, i) {
+                reading.push(path);
+                nodeBeginA[path] = 180 - a - d * i;
+                nodePositions[path] = new THREE.Vector3(
+                    pos.x + FOLDER_DISTANCE * Math.cos((a + d * i) * 0.0175), 0,
+                    pos.z + FOLDER_DISTANCE * Math.sin((a + d * i) * 0.0175)
+                );
+            });
+        }
+
+        // ///////////////////////记录所有文字物体
+        me.text3dHash = text3dHash;
+        // console.log(dataset);
+    }
+
+
+    // /////////////////////////////////////////////以下是工具方法
+    function findChildFolders(path) {
+        let result = [];
+        if (!fileTreeConfig[path]) {
+            return result;
+        }
+        let hash = fileTreeConfig[path];
+        Object.keys(hash).map(key => {
+            if (hash[key].isDirectory) {
+                result.push(hash[key].fullpath);
+            }
+        });
+        return result;
+    }
+
+    // function drawMap(me, font) {
+    //     const {renderer, scene} = me;
+    //     const file = fileFactory({
+    //         pos: new THREE.Vector3(500, 0, -500)
+    //     });
+    //     const folder = folderFactory({
+    //         pos: new THREE.Vector3(0, 0, 0)
+    //     });
+    //     const curve = curveFactory({
+    //         begin: new THREE.Vector3(0, 0, 0),
+    //         end: new THREE.Vector3(500, 0, -500)
+    //     });
+    //     const folderName = textFactory({
+    //         text: 'src',
+    //         font: font,
+    //         cameraAngleB: me.props.cameraAngleB,
+    //         isDirectory: true,
+    //         pos: new THREE.Vector3(0, 50, 0)
+    //     });
+    //     const fileName = textFactory({
+    //         text: 'main.js',
+    //         font: font,
+    //         cameraAngleB: me.props.cameraAngleB,
+    //         isDirectory: false,
+    //         pos: new THREE.Vector3(500, 50, -500)
+    //     });
+    //     const line = lineFactory({
+    //         begin: new THREE.Vector3(0, 0, 0),
+    //         end: new THREE.Vector3(500, 0, -500)
+    //     });
+    //     scene.add(file);
+    //     scene.add(folder);
+    //     scene.add(folderName);
+    //     scene.add(fileName);
+    //     scene.add(curve);
+    //     scene.add(line);
+    //     me.text3dHash = [folderName, fileName];
+    // }
+
+
+
+    // /////////////////////////////////////////////以下是3D物体生成工厂
 
     // 生成文字
     function textFactory(param) {
@@ -88,7 +175,6 @@ define(function (require) {
         return container;
     }
 
-
     // 生成目录节点
     function folderFactory(param) {
         let mesh = new THREE.Mesh(
@@ -99,7 +185,6 @@ define(function (require) {
         return mesh;
     }
 
-
     // 生成文件节点
     function fileFactory(param) {
         let mesh = new THREE.Mesh(
@@ -109,7 +194,6 @@ define(function (require) {
         mesh.position.copy(param.pos);
         return mesh;
     }
-
 
     // 生成节点间曲线
     function curveFactory(param) {
@@ -143,6 +227,14 @@ define(function (require) {
             return 200 + Math.random() * 100;
         }
         return curve.mesh;
+    }
+
+    // 生成节点直线
+    function lineFactory(param) {
+        const material = new THREE.LineBasicMaterial({color: 0xffffff});
+        const geometry = new THREE.Geometry();
+        geometry.vertices.push(param.begin, param.end);
+        return new THREE.Line(geometry, material);
     }
 
 
